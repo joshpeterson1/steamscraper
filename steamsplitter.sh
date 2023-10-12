57fb5b8ad785807183569ff5f819d7692ba1bd54
somber <joshpeterson6.0@gmail.com>
2023-10-12 01:12:10 -0600

    Update steamsplitter.sh
    Fixed ffmpeg command for splitting and changed final output message to inform of hex mods

--- steamsplitter.sh
+++ steamsplitter.sh
@@ -62,7 +62,7 @@ fi
 # Split the file into 5 strips
 for i in $(seq 0 4); do
     start_width=$((i * strip_width))
-    ffmpeg -i "$input_file" -vf "crop=${strip_width}:in_h:${start_width}:0" -an -sn -frames:v 1 $loop_option "${base_name}-split$((i+1)).$output_extension"
+    ffmpeg -i "$input_file" -vf "crop=${strip_width}:in_h:${start_width}:0" -an -sn $loop_option "${base_name}-split$((i+1)).$output_extension"
 done
 
 # Step 3: Modify hex values for the split files
@@ -70,4 +70,4 @@ for i in $(seq 1 5); do
     perl -0777 -pi -e 's/\x00(?!.*\x00).*$/\x01\x49\x45\x4E\x44\x00\xD1\x1A\x4F\xE1/s' "${base_name}-split${i}.$output_extension"
 done
 
-echo "File split into 5 vertical strips: ${base_name}-split1.$output_extension, ${base_name}-split2.$output_extension, ..., ${base_name}-split5.$output_extension"
+echo "File split into 5 vertical strips and hex changed for steam: ${base_name}-split1.$output_extension, ${base_name}-split2.$output_extension, ..., ${base_name}-split5.$output_extension"
